66cf082ca6481895a55eb5e204f8880691764d37
eltopoivi <lozanoivanx@gmail.com>
2025-08-24 18:52:48 +0200

    Create challenges.js

---
+++ utils/challenges.js
@@ -0,0 +1,14 @@
+export const challenges = [
+  {
+    description: "Crea un programa que imprima 'Hola Mundo'.",
+    tests: [{ input: "", expected: "Hola Mundo" }],
+  },
+  {
+    description: "Haz una función que sume dos números: 2 + 3.",
+    tests: [{ input: "", expected: "5" }],
+  },
+  {
+    description: "Escribe un bucle que imprima los números del 1 al 5.",
+    tests: [{ input: "", expected: "1\n2\n3\n4\n5" }],
+  },
+];
